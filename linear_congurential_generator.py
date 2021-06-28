bfb661fc9da84728286d183d0a702bb49a5c20c0
arian <aryan.senator@yahoo.com>
2021-06-28 22:26:33 +0430

    Update linear_congurential_generator.py

--- linear_congurential_generator.py
+++ linear_congurential_generator.py
@@ -4,6 +4,7 @@ import numpy, random, cv2
 class LCG():
     """ class for creating an LCG generator and assigning its values to an 2D array."""
 
+    @staticmethod
     def LCG(multiplier: int = None, increment: int = None, first_value: int = None, modulus: int = 256) -> int:
         if first_value is None:
             first_value = random.randint(0, 256)
@@ -11,4 +12,19 @@ class LCG():
             multiplier = random.randint(0, 256)
         if increment is None:
             increment = random.randint(0, 256)
-        
+        next_value = (multiplier * first_value + increment) % modulus
+        return next_value
+
+
+    
+    
+ra = numpy.zeros((512,512), numpy.uint8)
+lcg = LCG()
+
+with numpy.nditer(ra, op_flags = ["readwrite"]) as rai:
+    first_value = random.randint(0, 256)
+    rai[0][...] = first_value
+    rai.iternext()
+    for i in range(512*512):
+        rai[0][...] = lcg.LCG(first_value = first_value)
+        rai.iternext()
